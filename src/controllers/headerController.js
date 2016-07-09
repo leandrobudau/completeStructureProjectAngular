265ca6b63599b6b89d5b98ca9051cc039d8dab54
leandrobudau <leandrobudau@hotmail.com>
2016-07-08 22:39:01 -0300

    adding file model

--- src/controllers/headerController.js
+++ src/controllers/headerController.js
@@ -5,3 +5,14 @@ app.controller('headerController', function ($scope) {
         return 'funcao';  
     }
 });
+
+app.controller('upload', function ($scope, $http) {
+var fd = new FormData();
+        for (var key in $scope.topo)
+            fd.append(key, $scope.topo[key]);
+
+        return $http.post('http://localhost:8000/user', fd, {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        });
+})
